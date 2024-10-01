f5ca6e46007bda1f2aa75152559c8c23b39ee05f
Gbolahan Akande <geakande@gmail.com>
2024-10-01 20:35:50 +0100

    fixing requirement

--- routes/index.js
+++ routes/index.js
@@ -8,72 +8,71 @@ function controllerRouting(app) {
   const router = express.Router();
   app.use('/', router);
 
-  // App - Controller
+  // App Controller
 
-  // will return if redis server is alive
+  // should return if Redis is alive and if the DB is alive
   router.get('/status', (req, res) => {
     AppController.getStatus(req, res);
   });
 
-  // will return the number of users and files in the DB
+  // should return the number of users and files in DB
   router.get('/stats', (req, res) => {
     AppController.getStats(req, res);
   });
 
-  // User - Controller
+  // User Controller
 
-  // will create a new user in the DB
+  // should create a new user in DB
   router.post('/users', (req, res) => {
     UsersController.postNew(req, res);
   });
 
-  // will retrieve the user base on the token used
+  // should retrieve the user base on the token used
   router.get('/users/me', (req, res) => {
     UsersController.getMe(req, res);
   });
 
-  // Authentication - Controller
+  // Auth Controller
 
-  // will authenticate the user based on email and 
-  // password
+  // should sign-in the user by generating a new authentication token
   router.get('/connect', (req, res) => {
     AuthController.getConnect(req, res);
   });
 
-  // will authenticate the user based on token
+  // should sign-out the user based on the token
   router.get('/disconnect', (req, res) => {
     AuthController.getDisconnect(req, res);
   });
 
-  // Files - Controller
+  // Files Controller
 
-  // will upload a file to the server
+  // should create a new file in DB and in disk
   router.post('/files', (req, res) => {
     FilesController.postUpload(req, res);
   });
 
-  // will retrieve the file document based on the ID
+  // should retrieve the file document based on the ID
   router.get('/files/:id', (req, res) => {
     FilesController.getShow(req, res);
   });
 
-  // will retrieve all the file documents
-  // with a specific tag name and with pagination
+  // should retrieve all users file documents for a
+  // specific parentId and with pagination
   router.get('/files', (req, res) => {
     FilesController.getIndex(req, res);
   });
 
-  // will update the file document based on the ID
+  // should set isPublic to true on the file document based on the ID
   router.put('/files/:id/publish', (req, res) => {
     FilesController.putPublish(req, res);
   });
 
-  // will update the file document based on the ID
+  // should set isPublic to false on the file document based on the ID
   router.put('/files/:id/unpublish', (req, res) => {
     FilesController.putUnpublish(req, res);
   });
 
-  // will delete the file document based on the ID
+  // should return the content of the file document based on the ID
   router.get('/files/:id/data', (req, res) => {
     FilesController.getFile(req, res);
   });
